cc77d75540d4179f015d499b50f02e07a44fca24
노지용 <115959559+jiyongnoh@users.noreply.github.com>
2023-12-27 11:09:50 +0900

    feat: 메세지 누적 기능 추가

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ const inputField = chatBox.querySelector("input[type='text']");
 const button = chatBox.querySelector("button");
 const chatBoxBody = chatBox.querySelector(".chat-box-body");
 
+const messageArr = [];
+
+const RelUrl = "http://43.201.75.68:4000";
+const localUrl = "http://localhost:4000";
+
 button.addEventListener("click", sendMessage);
 inputField.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
@@ -12,6 +17,8 @@ inputField.addEventListener("keypress", function (event) {
 
 function sendMessage() {
   const message = inputField.value;
+  messageArr.push({ role: "user", content: message }); // 내가 쓴 메세지 저장
+
   inputField.value = "";
   chatBoxBody.innerHTML += `<div class="message">${message}</div>`;
   chatBoxBody.innerHTML += `<div id="loading" class="response loading">.</div>`;
@@ -21,27 +28,30 @@ function sendMessage() {
     var wait = document.getElementById("loading");
     if (window.dotsGoingUp && wait) wait.innerHTML += ".";
     else {
-      wait.innerHTML = wait.innerHTML.substring(1, wait.innerHTML.length);
+      wait.innerHTML = wait.innerHTML?.substring(1, wait.innerHTML.length);
       if (wait.innerHTML.length < 2) window.dotsGoingUp = true;
     }
     if (wait.innerHTML.length > 3) window.dotsGoingUp = false;
   }, 250);
 
-  fetch("http://43.201.75.68:4000/openAI/message", {
+  fetch(`${localUrl}/openAI/message`, {
     method: "POST",
     headers: {
       accept: "application.json",
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ message }),
+    body: JSON.stringify({ messageArr }),
   })
     .then((response) => {
       return response.json();
     })
     .then((data) => {
+      messageArr.push({ role: "assistant", content: data.message }); // 상담사 응답 메세지 저장
       document.getElementById("loading").remove();
       chatBoxBody.innerHTML += `<div class="response">${data.message}</div>`;
       scrollToBottom();
+
+      // console.log(messageArr);
     });
 }
 
